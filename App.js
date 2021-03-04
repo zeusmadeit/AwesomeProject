5e4bd64e46b3b30d23f1389deb7ba5e47b9305de
Abraham Tornegyen <codexapp44@gmail.com>
2021-03-04 00:40:06 +0000

    Header Component and FlatList added

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
-import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-      <CarItem 
-        name={'Model S'}
-        tagline={'Order Online For'}
-        taglineCTA={'Touchless Delivery'}
-        image={require('./assets/images/ModelS.jpeg')}
-      />
-
+      <Header/>
+      <CarsList/>
       <StatusBar style="auto" />
     </View>
   );
